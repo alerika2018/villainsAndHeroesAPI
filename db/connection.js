4c1f1f6c37b9fc8e3eae707175f1128dc150fe1f
Alejandra Hernandez <44346060+alerika2018@users.noreply.github.com>
2021-11-06 14:55:56 -0700

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,7 @@
-// const mongoose = require('mongoose')
 
-// const db = `mongodb+srv://alerika:shannon7@cluster0.id1ka.mongodb.net/endgame?retryWrites=true&w=majority`;
-
-// mongoose.connect(db)
-
-// const dbConnected = mongoose.connection
-// dbConnected.on("error",console.error.bind(console, "MongoDB connection error:"))
-// module.exports = dbConnected
 
 const mongoose = require("mongoose");
 
-let mongoDB = `mongodb+srv://alerika:shannon7@cluster0.id1ka.mongodb.net/endgame?retryWrites=true&w=majority`;
+let mongoDB = `mongodb+srv://user:password@cluster0.id1ka.mongodb.net/database_name?retryWrites=true&w=majority`;
 
 module.exports = mongoose.connect(mongoDB);
